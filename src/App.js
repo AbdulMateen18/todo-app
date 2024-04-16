02a72e36f02232e08f3a9f40ee1ef92ecf041340
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-16 20:31:31 +0500

    added todolist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import TodoSearch from "./components/TodoSearch";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 0, task: "Learn JavaScript", status: "Active" },
+    { id: 1, task: "Read a self-help book", status: "Active" },
+    { id: 2, task: "Play PS5", status: "Active" },
+    { id: 3, task: "Watch YouTube videos", status: "Active" },
+    // { id: 5, task: "Pray to God", status: "Active" },
+  ]);
+
   return (
     <div className="todo-container">
       <TodoSearch />
       <TodoFilter />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="todo-list">
-      <div className="todo-list-item">
-        <div className="task">
-          <input type="checkbox" />
-          <p>todo item</p>
-        </div>
+      {todos.map((todo) => (
+        <div className="todo-list-item" key={todo.id}>
+          <div className="task">
+            <input type="checkbox" />
+            <p>{todo.task}</p>
+          </div>
 
-        <div className="btn-container">
-          <div className="edit">
-            <FaRegEdit />
-          </div>
-          <div className="del">
-            <MdDelete />
+          <div className="btn-container">
+            <div className="edit">
+              <FaRegEdit size={25} />
+            </div>
+            <div className="del">
+              <MdDelete size={25} />
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
