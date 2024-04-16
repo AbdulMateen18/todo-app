484a56d690840e7d6e8c9dcba87d192acedaa53b
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-04-16 19:41:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoSearch from "./components/TodoSearch";
+import TodoFilter from "./components/TodoFilter";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-container">
-      
+      <TodoSearch />
+      <TodoFilter />
+      <TodoList />
     </div>
   );
 }
